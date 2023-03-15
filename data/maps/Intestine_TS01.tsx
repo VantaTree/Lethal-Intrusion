594149e3b5eef731812058be29b7d07bbf587384
Zixi Liu <59849551+ForXample@users.noreply.github.com>
2023-03-14 20:33:26 -0400

    [design] Intestine Area level finished.

---
+++ data/maps/Intestine_TS01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Intestine" tilewidth="16" tileheight="16" tilecount="30" columns="6">
+ <image source="../../../../Users/viczi/Downloads/Tilesets_Intestine.png" width="100" height="80"/>
+</tileset>
